0d6a7d37b800f661f195b6c7f662f356b4be2380
kodebytes <81661316+kodebytes@users.noreply.github.com>
2021-06-08 18:51:28 -0500

    finished warmups day 1

--- warmups/arrays.js
+++ warmups/arrays.js
@@ -1,1 +1,34 @@
 // Day 1
+let foods = ["apple", "orange", "rice", "bread", "tofu"];
+let allergies = ["gluten", "soy"];
+
+for (let i = foods.length - 1; i >= 0; i--) {
+  console.log(foods[i][0].toUpperCase() + foods[i].substring(1));
+}
+
+const checkLength = (value) => {
+    if (value.length > 4 ){
+        return true;
+    }
+    return false;
+}
+
+const allergyCheck = (value) => {
+
+}
+
+for( x of foods){
+    console.log(checkLength(x))
+}
+
+let cards = [2, 8, "K", 9, 10, 3, 4, "Q", 7, "J", 5, 6, "A"];
+let faceCards = []
+let numberedCards = []
+
+for (let x of cards ){
+    isNaN(x) === true ? faceCards.push(x):
+    numberedCards.push(x);
+};
+
+console.log(faceCards);
+console.log(numberedCards);
