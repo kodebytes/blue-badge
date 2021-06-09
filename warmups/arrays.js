e47387888eb37a645ca72a913ae02fcfd0691773
kodebytes <81661316+kodebytes@users.noreply.github.com>
2021-06-08 19:17:04 -0500

    4th warmup for day 1

--- warmups/arrays.js
+++ warmups/arrays.js
@@ -7,28 +7,44 @@ for (let i = foods.length - 1; i >= 0; i--) {
 }
 
 const checkLength = (value) => {
-    if (value.length > 4 ){
-        return true;
-    }
-    return false;
-}
+  if (value.length > 4) {
+    return true;
+  }
+  return false;
+};
 
-const allergyCheck = (value) => {
+const allergyCheck = (value) => {};
 
-}
-
-for( x of foods){
-    console.log(checkLength(x))
+for (x of foods) {
+  console.log(checkLength(x));
 }
 
 let cards = [2, 8, "K", 9, 10, 3, 4, "Q", 7, "J", 5, 6, "A"];
-let faceCards = []
-let numberedCards = []
+let faceCards = [];
+let numberedCards = [];
 
-for (let x of cards ){
-    isNaN(x) === true ? faceCards.push(x):
-    numberedCards.push(x);
-};
+for (let x of cards) {
+  isNaN(x) ? faceCards.push(x) : numberedCards.push(x);
+}
 
 console.log(faceCards);
 console.log(numberedCards);
+
+let animals = ["bat", "tiger", "yak", "monkey", "snake", "lizard", "panda", "monkey", "hyena"];
+
+let zooTrip = {
+  seen: [],
+  animalsSeen: 0,
+};
+
+const seenAtZoo= (animal) => {
+  zooTrip.seen.push(animal);
+};
+
+animals.forEach((animal) => {
+  //console.log(`${animal} was seen today.`);
+  !zooTrip.seen.includes(animal) ? zooTrip.animalsSeen++ : seenAtZoo(animal);
+  seenAtZoo(animal);
+});
+
+console.log(zooTrip);
